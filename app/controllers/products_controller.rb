cb23ee412d03534b2d9aef0c8a3d2484e54ce7ce
Miguel601 <henriquez.miguel@outlook.com>
2019-04-15 23:32:42 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+
+    def index
+        @product = session[:cart]
+    end
+
+    def add 
+        session[:cart] ||= [] << params[:product]
+        redirect_to '/'
+    end
+
+
+
+
+end
